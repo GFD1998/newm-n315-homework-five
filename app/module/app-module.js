29ee4e490f33f113c5bfaa7bdc614843f8d6cbb5
GFD1998 <gabrieldennett@gmail.com>
2023-03-14 22:06:51 -0400

    General Update

---
+++ app/module/app-module.js
@@ -0,0 +1,6 @@
+
+import UserSettings from "./user-settings.js";
+import SPAManager from "../SPAManager.js";
+import Model from "./model-module.js";
+
+var whiteRabbit = new SPAManager(new Model(new UserSettings));
